ba92924e568f8f8733031c45a89328c2ea34ab75
Jakub Žitný <jakubzitny@avocode.com>
2018-11-21 13:21:45 +0100

    add snapshot test examples

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import chai, { expect } from 'chai'
+import chaiJestSnapshot from 'chai-jest-snapshot'
+import chaiEnzyme from 'chai-enzyme'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+Enzyme.configure({ adapter: new Adapter() })
+chai.use(chaiEnzyme())
+chai.use(chaiJestSnapshot)
+
+
+describe('App', () => {
+  it('renders without crashing with ReactDOM', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+
+  it('renders shallowly with correct text', () => {
+    const wrapper = shallow(<App />)
+
+    console.log(wrapper.debug())
+
+    expect(wrapper).to.have.descendants('Header')
+  })
+
+
+  it('renders correct text', () => {
+    const wrapper = mount(<App />)
+
+    console.log(wrapper.debug())
+
+    expect(wrapper).to.contain.text('Ahoj, webdev!')
+  })
+
+
+  it('renders fully correctly', () => {
+    const wrapper = mount(<App />)
+
+    console.log(wrapper.debug())
+
+    expect(wrapper).to.matchSnapshot()
+  })
+})
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <p>
+          Ahoj, webdev!
+        </p>
+      </header>
+    );
+  }
+}
