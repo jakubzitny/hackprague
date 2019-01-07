291d873a43ef3ca44a1ae6a0be15882795a68ebf
Jakub Žitný <jakubzitny@avocode.com>
2019-01-07 04:13:55 +0100

    Updates

---
+++ precache-manifest.7d5cdff2259c90a0de95bf06ab0b52cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a7fe66953035b14f8c6",
+    "url": "/hackprague/static/js/runtime~main.3a7fe669.js"
+  },
+  {
+    "revision": "296a9fdac2b4bc21f603",
+    "url": "/hackprague/static/js/main.296a9fda.chunk.js"
+  },
+  {
+    "revision": "f2b9cdb11466b997976d",
+    "url": "/hackprague/static/js/1.f2b9cdb1.chunk.js"
+  },
+  {
+    "revision": "296a9fdac2b4bc21f603",
+    "url": "/hackprague/static/css/main.2468631b.chunk.css"
+  },
+  {
+    "revision": "297e67f49f31278aba10de7e92623667",
+    "url": "/hackprague/index.html"
+  }
+];
--- src/config/speakers.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import { List } from 'immutable'
-
-export const SPEAKERS = List.of(
-  'Jirka',
-  'Honza',
-  'Nikita'
-)
