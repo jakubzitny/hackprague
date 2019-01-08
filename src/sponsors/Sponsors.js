8f65ca84b3bd419643206f359c9e7b7cf171c44d
Jakub Žitný <jakubzitny@avocode.com>
2019-01-09 00:55:56 +0100

    add sponsors

--- src/sponsors/Sponsors.js
+++ src/sponsors/Sponsors.js
@@ -1,15 +1,60 @@
 import React, { Component } from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
-import WrappedContainer from '../WrappedContainer'
+const SponsorsWrapper = styled.div`
+  height: 400px;
+  width: 1200px;
 
+  margin: 80px auto;
+  text-align: left;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: left;
+`
+
+const SponsorSection = styled.div`
+  margin: 40px 0;
+
+  display: flex;
+  flex-direction: row;
+`
+
+const Sponsor1 = styled.div`
+  width: 500px;
+  height: 129px;
+  background-image: url('sponsors/IPRlogo.png');
+  background-size: 500px 129px;
+  margin: 0 40px;
+`
+const Sponsor2 = styled.div`
+  width: 416px;
+  height: 129px;
+  background-image: url('sponsors/OICTLogo.png');
+  background-size: 416px 129px;
+  margin: 0 40px;
+`
 
 export default class Sponsors extends Component {
   render() {
     return (
-      <WrappedContainer squared={false}>
-        <div className="title item">Sponsors & Partners</div>
-      </WrappedContainer>
+      <SponsorsWrapper>
+        <TitleWrapper>
+          <div className="title item">Sponsors & Partners</div>
+        </TitleWrapper>
+
+        <SponsorSection>
+          <Sponsor1 />
+          <Sponsor2 />
+        </SponsorSection>
+      </SponsorsWrapper>
     )
   }
 }
