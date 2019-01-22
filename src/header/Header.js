f63fe96ef6b6157a8ecd2d18dce6292a4f6b0785
Jakub Žitný <jakubzitny@avocode.com>
2019-01-22 11:30:40 +0100

    remove unused calendar from header

--- src/header/Header.js
+++ src/header/Header.js
@@ -85,29 +85,6 @@ const ApplyButton = styled.button`
   }
 `
 
-const Calendar = styled.div`
-  &.addeventatc {
-    width: 160px !important;
-
-    display: inline-block;
-    position: relative;
-    text-align: center;
-    cursor: pointer;
-    font-family: "Open Sans", Roboto, "Segoe UI", Frutiger, "Frutiger Linotype",
-      "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-    font-size: 15px;
-    color: var(--main-color) !important;
-    font-weight: 600;
-    text-decoration: none;
-    padding: 12px;
-    text-transform: uppercase;
-  }
-
-  & .addeventatc_icon {
-    display: none;
-  }
-`
-
 const HeaderRight = styled.div`
   flex: 1;
   display: flex;
@@ -255,16 +232,6 @@ export default class Header extends Component {
             <ButtonWrapper>
               <ApplyButton onClick={this.props.onApplyClick}>Apply</ApplyButton>
             </ButtonWrapper>
-            {/* <ButtonWrapper>
-              <Calendar title="Add to Calendar" className="addeventatc" style={{width: '160px !important', boxSizing: 'border-box', borderRadius: 0}}>
-                Add to Calendar
-                <span className="start">05/04/2019 09:00</span>
-                <span className="end">05/05/2019 18:00</span>
-                <span className="timezone">Europe/Prague</span>
-                <span className="title">HackPrague 2019</span>
-                <span className="location">Vyšehradská 2075/51, 128 00 Nové Město</span>
-              </Calendar>
-            </ButtonWrapper> */}
           </HeaderLeft>
           <HeaderRight>
             <City />
